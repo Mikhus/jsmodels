2b58643f91ff987b7b5ea32c4181cee4a4f07ca2
Mykhailo Stadnyk <mikhus@gmail.com>
2016-06-12 10:47:10 +0300

    Code layout re-factored

--- model/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.assign({
-    Schema: require('./Schema'),
-    ValueObject: require('./ValueObject'),
-    ValueArray: require('./ValueArray'),
-    Log: require('./Log')
-});
