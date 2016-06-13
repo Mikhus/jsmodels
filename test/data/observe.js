5839a13d9abc9d173f387ca4953175ce91eae039
Mykhailo Stadnyk <mikhus@gmail.com>
2016-06-13 22:45:55 +0300

    Yet another re-factoring stage + some code optimizations

--- test/data/observe.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-const sinon = require('sinon');
-const observe = require('../../lib/data/observe');
-
-describe('observe()', () => {
-
-});
--- test/data/uuid.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-const sinon = require('sinon');
-const uuid = require('../../lib/data/uuid');
-
-describe('uuid()', () => {
-
-});
