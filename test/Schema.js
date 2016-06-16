5f0fd3a52e8a1ba3bcaef3776c86e25d5fd99243
Mykhailo Stadnyk <mikhus@gmail.com>
2016-06-16 19:14:11 +0300

    Re-factoring - split core functions to a separate basic module

--- test/Schema.js
+++ test/Schema.js
@@ -7,11 +7,6 @@ describe('Schema', () => {
     it('should have options defined', () => {
         expect(Schema.options).not.to.be.undefined;
     });
-    it('should have refer Log through read-only log property', () => {
-        expect(Schema.log).to.equal(Log);
-        expect(() => Schema.log = console).to.not.throw;
-        expect(Schema.log).to.equal(Log);
-    });
 
     describe('Schema.create()', () => {
         it('Should return object of type Schema', () => {
