c8f3d7d952ad98819891b6f3c17d5a204a3522f4
Mykhailo Stadnyk <mikhus@gmail.com>
2016-06-24 15:36:30 +0300

    Observer test coverage improved, +bugfixing

--- test/specs/Observer.js
+++ test/specs/Observer.js
@@ -3,6 +3,8 @@ const sinon = require('sinon');
 const Schema = require('../../lib/Schema');
 const Observer = require('../../lib/Observer');
 const Subscriber = require('../mocks/Subscriber');
+const jsSchemas = require('../data/js-schemas');
+const fs = require('fs');
 
 describe('Observer', () => {
     it('should have options defined', () => {
@@ -39,12 +41,150 @@ describe('Observer', () => {
             )).to.not.throw(Error);
         });
 
-        it('should return primitive value for primitive schemas', () => {
+        it('should observe complex data recursively', () => {
+            let schema = Schema.create(jsSchemas[0]);
+            let errors = [];
+            let subscriber = new Subscriber();
+            let data = Observer.observe({
+                firstName: '',
+                lastName: '',
+                email: '',
+                rates: [],
+                addresses: []
+            }, schema, errors, subscriber);
 
+            expect(data.__isObserved__).to.be.true;
+            expect(data.rates.__isObserved__).to.be.true;
+            expect(data.addresses.__isObserved__).to.be.true;
         });
 
-        it('should return observed proxy for complex schemas', () => {
-            
+        it('should raise proper errors on data invalidate', () => {
+            let schema = Schema.create(jsSchemas[0]);
+            let errors = [];
+            let subscriber = new Subscriber();
+            let data = Observer.observe({
+                firstName: '',
+                lastName: '',
+                email: '',
+                rates: [],
+                addresses: []
+            }, schema, errors, subscriber);
+
+            data.firstName = 20;
+
+            expect(errors[0].code).to.be.equal(Observer.ERROR_TYPE);
+
+            delete data.lastName;
+
+            expect(errors[1].code).to.be.equal(Observer.ERROR_REQUIRED);
+        });
+
+        it('should emit invalidate events on subscribes when invalid data is ' +
+            'assigned', () =>
+        {
+            let schema = Schema.create(jsSchemas[0]);
+            let errors = [];
+            let subscriber = new Subscriber();
+            let data = Observer.observe({
+                firstName: '',
+                lastName: '',
+                email: '',
+                rates: [],
+                addresses: []
+            }, schema, errors, subscriber);
+            let spy = sinon.spy(subscriber, 'emit');
+
+            data.firstName = 20;
+
+            expect(spy.calledWith('invalidate', errors[0])).to.be.true;
+        });
+
+        it('should generate errors with full trace if specified', () => {
+            Observer.options.fullTrace = true;
+
+            let schema = Schema.create(jsSchemas[0]);
+            let errors = [];
+            let subscriber = new Subscriber();
+            let data = Observer.observe({
+                firstName: '',
+                lastName: '',
+                email: '',
+                rates: [],
+                addresses: []
+            }, schema, errors, subscriber);
+
+            data.firstName = 20;
+
+            expect(errors[0]).to.have.property('stack');
+            expect(errors[0].stack).to.be.instanceof(Array);
+            expect(errors[0].stack.length).to.be.above(0);
+            Observer.options.fullTrace = false;
+        });
+        
+        it('should not generate any trace info if it is forbidden', () => {
+            Observer.options.traceErrors = false;
+            Observer.options.fullTrace = true;
+
+            let schema = Schema.create(jsSchemas[0]);
+            let errors = [];
+            let subscriber = new Subscriber();
+            let data = Observer.observe({
+                firstName: '',
+                lastName: '',
+                email: '',
+                rates: [],
+                addresses: []
+            }, schema, errors, subscriber);
+
+            data.firstName = 20;
+
+            expect(errors[0]).not.to.have.property('fileName');
+            expect(errors[0]).not.to.have.property('lineNumber');
+            expect(errors[0]).not.to.have.property('columnNumber');
+            expect(errors[0]).not.to.have.property('stack');
+
+            Observer.options.traceErrors = true;
+            Observer.options.fullTrace = false;
+        });
+        
+        it('should raise error with the proper trace', () => {
+            let schema = Schema.create(jsSchemas[0]);
+            let errors = [];
+            let subscriber = new Subscriber();
+            let data = Observer.observe({
+                firstName: '',
+                lastName: '',
+                email: '',
+                rates: [],
+                addresses: []
+            }, schema, errors, subscriber);
+
+            data.firstName = 20;
+
+            expect(errors[0].fileName).to.be.equal(fs.realpathSync(__filename));
+            expect(errors[0].lineNumber).to.be.equal(162);
+            expect(errors[0].columnNumber).to.be.equal(28);
+        });
+
+        it('should raise errors for internal recursively for complex ' +
+            'schemas', () =>
+        {
+            let schema = Schema.create(jsSchemas[0]);
+            let errors = [];
+            let subscriber = new Subscriber();
+            let data = Observer.observe({
+                firstName: '',
+                lastName: '',
+                email: '',
+                rates: [],
+                addresses: []
+            }, schema, errors, subscriber);
+
+            data.addresses.push({});
+
+            expect(errors.length).to.be.equal(3);
+            errors.forEach(err =>
+                expect(err.code).to.be.equal(Observer.ERROR_REQUIRED));
         });
     });
 
