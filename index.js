06ca691502c646a09f211986ca652c66dea4e962
Mykhailo Stadnyk <mikhus@gmail.com>
2016-12-12 13:16:40 +0200

    Pure models

--- index.js
+++ index.js
@@ -3,9 +3,5 @@ module.exports = Object.assign(require('./lib/BaseModel'), {
     Observer: require('./lib/Observer'),
     EventEmitter: require('./lib/EventEmitter'),
     Log: require('./lib/Log'),
-    UUID: require('./lib/UUID'),
-    Channel: require('./lib/Channel'),
-    BaseChannel: require('./lib/channel/Base'),
-    WebSocketChannel: require('./lib/channel/WebSocket'),
-    FirebaseChannel: require('./lib/channel/Firebase')
+    UUID: require('./lib/UUID')
 }, require('./lib/core'));
